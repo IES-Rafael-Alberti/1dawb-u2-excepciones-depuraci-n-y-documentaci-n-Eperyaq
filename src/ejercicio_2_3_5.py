92ead46ef68cae846019c38b3537de845106672f
eperyaq205 <eperyaq205@g.educaand.es>
2023-11-07 16:10:05 +0100

    cambio

---
+++ src/ejercicio_2_3_5.py
@@ -0,0 +1,14 @@
+"""
+Escribir que solicite una contraseña, y si no coincide con la que se tiene, lance la excepción NameError con el mensaje, "Incorrect Password!!"
+"""
+
+
+
+
+def main():
+    
+    print()
+
+
+if __name__ == "__main__":
+    main()
