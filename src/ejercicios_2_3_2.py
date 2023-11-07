a6b7080678f65cd435f4985b0f3e9b71750a8b06
eperyaq205 <eperyaq205@g.educaand.es>
2023-11-07 17:32:37 +0100

    si

---
+++ src/ejercicios_2_3_2.py
@@ -0,0 +1,21 @@
+"""
+Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.
+"""
+
+def pedir ():
+    numero = int(input(("Dime un numero entero positivo porfavor.")))
+
+    if numero <= 0:
+        raise Exception("no seas asi, pon el numero bien")
+    
+    return numero
+
+
+
+
+def main():
+    print(pedir())
+
+
+if __name__=="__main__":
+    main()
